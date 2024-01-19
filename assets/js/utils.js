5ccbfa4a5f0c8feeebee498afce61adae87fe1c4
AlienUrbano <paulogabrielneves@hotmail.com>
2024-01-19 07:58:53 -0300

    feat: creating a utils modules who toggle pokemon card class toggles.

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,5 +1,22 @@
-function showPokemonModal(pokemonNumber) {
-    
-    alert(`${pokemonNumber}`);
+function toggleCardExpansion(pokemonNumber) {
 
-}
+    const pokemonCard = document.getElementById(`pokemonCard-${pokemonNumber}`);
+        
+            if (pokemonCard) {
+                pokemonCard.classList.toggle('expanded');
+                showStats(pokemonNumber)
+            }
+
+
+ }
+
+ function showStats(pokemonNumber) {
+
+    const statsContainer = document.getElementById(`stats-container-${pokemonNumber}`);
+        
+            if (statsContainer) {
+                statsContainer.classList.toggle('show');               
+            }
+
+
+ }
